44f49d22e8444f96d8f94d245599b82c6ef7e3d0
Aloísio M. Lingo Filho <alingofilho@gmail.com>
2023-03-22 09:17:45 -0300

    ex04

--- ex04.c
+++ ex04.c
@@ -2,12 +2,12 @@
 #include <stdlib.h>
 
 int main(){
-    printf("\t\t\t\t\t\t*");
-    printf("\n\t\t\t\t\t*");
-    printf("\n\t\t\t\t*");
-    printf("\n*\t\t\t*");
-    printf("\n\t*\t*");
-    printf("\n\t\t*");
+    printf("       *");
+    printf("\n      *");
+    printf("\n     *");
+    printf("\n*   *");
+    printf("\n * *");
+    printf("\n  *");
 
     return 0;
 
