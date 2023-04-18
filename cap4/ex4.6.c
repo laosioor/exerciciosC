809af4609a6332dccf5bbd45614b9ed63a776532
laosioor <alingofilho@gmail.com>
2023-04-18 13:02:51 -0300

    ex4.6.c

--- cap4/ex4.6.c
+++ cap4/ex4.6.c
@@ -5,7 +5,7 @@
 int main() {
     int n[5];
     int p[5];
-    int b;
+    int b, q = 0;
     
     for (int i = 0; i < 5; i++) {
         printf("array[%d]: ", i);
@@ -18,20 +18,40 @@ int main() {
 
     for (int i = 0; i < 5; i++) {
         if (n[i] == b) {
-            for (int j = 0; j < 5; j++) {
-                if (p[j] != 0) {
-                    continue;
-                }
-
+            for (int j = q; j < 5; j++) {
                 p[j] = i;
+                q++;
+                break;
             }
         }
-    }    
-
-    int t = (int) (sizeof(p[]) / sizeof(p[0]));
-
-    for (int i = 0; i < t; i++) {
-        printf("foi encontrado no indice: %d", p[i]);
     }
+
+    if (q == 0) {
+        printf("O array nao contem o valor %d.", b);
+    } 
+    else {
+        printf("O valor %d foi encontrado ", b);
+
+        if (q == 1) {
+            printf("no indice ");
+        } else {
+            printf("nos indices ");
+        }
+
+        for (int i = 0; i < q; i++) {
+            printf("%d", p[i]);
+
+            if (i + 1 == q) {
+                break;
+            } else if (i+1 == q-1) {
+                printf(" e ");
+            } else {
+                printf(", ");
+            }
+        }
+
+        printf(" do array.");
+    }
+
     return 0;
 }
